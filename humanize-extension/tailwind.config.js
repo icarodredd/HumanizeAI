d39f5181c75fab737040a4719f29bb3fe05b76d6
icarodredd <icarog854@gmail.com>
2024-06-08 11:51:23 -0300

    feat: popup 1/3

---
+++ humanize-extension/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        hiapurple: "#5940EB",
+      },
+      fontFamily: {
+        poppins: ["poppins", "Arial"],
+      },
+    },
+  },
+  plugins: [],
+};
